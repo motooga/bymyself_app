176903e61e08e6e86fd2a465118e395ab55ebfb1
motooga <hashioga2017@gmail.com>
2023-10-29 07:47:32 +0900

    デプロイエラー解消テスト3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,8 @@
 set -o errexit
 
 bundle install
-yarn install
 bundle exec rake assets:precompile
-# bundle exec rake assets:clean
+bundle exec rake assets:clean
 # デプロイのデータベース関係でエラーがあったときは↓をコメントアウトしてコメントアウトになっているのを戻す
 bundle exec rake db:migrate
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
