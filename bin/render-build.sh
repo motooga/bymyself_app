f273eef33fdb112570048882b57bcc0bdae9e11d
motooga <hashioga2017@gmail.com>
2023-12-14 05:26:02 +0900

    DBのリセット

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,5 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # デプロイのデータベース関係でエラーがあったときは↓をコメントアウトしてコメントアウトになっているのを戻す
-bundle exec rake db:migrate
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
